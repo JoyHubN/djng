a23d5bd80918e023b741295b39f7d6a7d3ad128c
nik <n.cowalyow2017@yandex.ru>
2026-03-10 11:26:33 +0300

    админка

--- my_site/music/admin.py
+++ my_site/music/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+admin.site.site_header = 'Панель администрирования'
+
+
+class PostAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('text', 'pub_date', 'author')
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+admin.site.register(Post, PostAdmin)  
