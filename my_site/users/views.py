9a641297890b12caf6eda5cd6b7b1bfe3ec07138
nik <n.cowalyow2017@yandex.ru>
2026-03-31 10:38:18 +0300

    добавлена регистрация

--- my_site/users/views.py
+++ my_site/users/views.py
@@ -8,20 +8,16 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 
 def login_user(request):
-    # active_sessions = Session.objects.filter(expire_date__gt=timezone.now())
-    session_id = request.COOKIES.get('sessionid')
-    if session_id:
-        session = Session.objects.get(session_key=session_id)
-        data = session.get_decoded()
-        
-        if data:
-            return redirect(reverse('music:index'))    
+    
+    if request.user.is_authenticated:
+        return redirect('music:index')  
 
     if request.method ==  'POST':
         form = LoginUserForm(request.POST)
+        
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request, username=cd['username'], password=cd['password'])
@@ -30,7 +26,13 @@ def login_user(request):
                 login(request, user)
                 return HttpResponseRedirect(reverse('music:index'))
             else:
-                return render(request, 'users/login.html', {'form': form, 'message': 'Неверный логин или пароль'})
+                return render(request, 
+                              'users/login.html', 
+                              {
+                                  'form': form, 
+                                  'message': 'Неверный логин или пароль'
+                                }
+                        )
 
     else:
         form = LoginUserForm()
@@ -40,3 +42,22 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('music:index'))
+
+
+def registration(request):
+    
+    if request.user.is_authenticated:
+        return redirect('music:index')
+
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:        
+        form = RegisterUserForm()
+    return render(request, 'users/registration.html', {'form': form})
